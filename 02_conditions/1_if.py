5d374c2f0705e82c73953e57e16d71e5dd7ec370
Asad <asadshah1066@gmail.com>
2025-09-04 22:59:05 +0530

    conditions

---
+++ 02_conditions/1_if.py
@@ -0,0 +1,6 @@
+# Mini Story
+
+kettle_boiled = False
+
+if kettle_boiled:  # only run when condition is true
+    print("Kellle Done! time to make Chai")
---
+++ 02_conditions/5_ternary_operator.py
@@ -0,0 +1,7 @@
+# Delevery fees waiver
+
+order_amount = int(input("Enter the order amount: "))
+
+delivery_fees = 0 if order_amount > 300 else 30
+
+print(f"Delivery fees is : {delivery_fees}")
