c94169aae0e359342d98966586f2e98a980e5d9b
Asad <asadshah1066@gmail.com>
2025-10-10 12:16:54 +0530

    some correnciton

--- 09_exception/6_file_handling.py
+++ 09_exception/6_file_handling.py
@@ -1,14 +1,15 @@
-# file = open("order.txt", "w")
+# Without 'with' (requires manual closing)
 
+# file = open("example.txt", "w")
 # try:
-#     file.write("Hello my name is asad nice to meet you")
-# except:
-#     print("Error in file handling")
+#     file.write("Hello, world!")
 # finally:
 #     file.close()
 
-
-# [optimization method]
-
-with open("greet2.txt","w") as file:
-    file.write("Hello from asad side")
+# With 'with' (automatic closing)
+with open("example.txt", "w") as file:
+    file.write("Hello, world!")
+    
+"""
+In the with example, the open() function returns a file object that is a context manager. When the with block is entered, the file is opened. When the block is exited (either normally or due to an error), the __exit__ method of the file object is called, automatically closing the file.
+"""
